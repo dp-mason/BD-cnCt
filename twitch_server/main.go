f7c8557c3bd53fd935277607abba8b9eb1cb9d85
dp-mason <davidpattenmason@gmail.com>
2024-07-31 23:21:46 -0600

    server needs some work displaying real chat logs and sending actual chat logs to the vcv rack module, vcv rack module has issue where is freezes, is also not able to fetch the json for some reason even though the server shows that it is being sent.

--- twitch_server/main.go
+++ twitch_server/main.go
@@ -1,23 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"github.com/gempir/go-twitch-irc/v4"
+	"log"
+	"net/http"
+	"time"
 )
 
-func main() {
-	// or client := twitch.NewAnonymousClient() for an anonymous user (no write capabilities)
-	client := twitch.NewClient("yourtwitchusername", "oauth:123123123")
+// func twitchWorker(chatUpdates chan string) {
+// 	// or client := twitch.NewAnonymousClient() for an anonymous user (no write capabilities)
+// 	client := twitch.NewClient("yourtwitchusername", "oauth:123123123")
 
-	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-		fmt.Println(message.Message)
+// 	client.OnClearChatMessage(func(message twitch.ClearChatMessage) {
+// 		fmt.Println(message.Message)
+// 		chatUpdates <- message.Message
+// 	})
+
+// 	client.Join("dpmason")
+
+// 	err := client.Connect()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+func fakeTwitchWorker(chatUpdates chan string) {
+	for {
+		time.Sleep(time.Second)
+		chatUpdates <- "foobar"
+		fmt.Println("fake twitch worker: foobar")
+	}
+}
+
+type ChatUpdateResp struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+}
+
+func jsonServerWorker(chatUpdates chan string) {
+	http.HandleFunc("/chat-queue", func(respWriter http.ResponseWriter, req *http.Request) {
+		// construct the json string
+		for msg := range chatUpdates {
+			fmt.Println("JSON server:", msg)
+		}
+
+		// Create an instance of the response struct
+		response := ChatUpdateResp{
+			Message: "{Hello: World}",
+			Status:  200,
+		}
+
+		respWriter.Header().Set("Content-Type", "application/json")
+
+		// Encode the response struct to JSON
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			// Handle the error and return an HTTP 500 status code
+			http.Error(respWriter, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		respWriter.Write(jsonResponse)
 	})
 
-	client.Join("gempir")
+	fmt.Println("Server is running at 0.0.0.0:4554")
+	log.Fatal(http.ListenAndServe("0.0.0.0:4554", nil))
+}
 
-	err := client.Connect()
-	if err != nil {
-		panic(err)
-	}
+func main() {
+	chatUpdates := make(chan string, 16)
+	//go twitchWorker(chatUpdates)
+	go fakeTwitchWorker(chatUpdates)
+	go jsonServerWorker(chatUpdates)
+
+	select {}
 }
