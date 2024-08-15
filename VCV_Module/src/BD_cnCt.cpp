45bcd8ec87f4cd03293a458e01440c306a3de1f4
dp-mason <davidpattenmason@gmail.com>
2024-08-15 00:11:13 -0600

    visually complete, need to hook up inputs and outputs and such

--- VCV_Module/src/BD_cnCt.cpp
+++ VCV_Module/src/BD_cnCt.cpp
@@ -110,6 +110,7 @@ struct BD_cnCt : Module {
 		OUTPUTS_LEN
 	};
 	enum LightId {
+		TEST_LIGHT,
 		LIGHTS_LEN
 	};
 
@@ -219,87 +220,102 @@ struct BD_cnCt : Module {
 
 
 struct BD_cnCtWidget : ModuleWidget {
+	BGPanel *pBackPanel;
+	
 	BD_cnCtWidget(BD_cnCt* module) {
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/BD_cnCt.svg")));
 
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		// Adapted from https://github.com/netboy3/MSM-vcvrack-plugin/
+		pBackPanel = new BGPanel();
+		pBackPanel->box.size = box.size;
+		pBackPanel->imagePath = asset::plugin(pluginInstance, "res/BD_cnCt.png");
+		pBackPanel->visible = false;
+		addChild(pBackPanel);
+		pBackPanel->visible = true;
+
+		// addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		// addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		// addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		// addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(12.952, 13.75)), module, BD_cnCt::PORT_KNOB_ZERO_PARAM));
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(28.388, 13.75)), module, BD_cnCt::PORT_KNOB_ONE_PARAM));
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(43.823, 13.75)), module, BD_cnCt::PORT_KNOB_TWO_PARAM));
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(59.259, 13.75)), module, BD_cnCt::PORT_KNOB_THREE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(116.678, 31.609)), module, BD_cnCt::TEST_ENABLE_BUTTON_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(134.017, 37.179)), module, BD_cnCt::TEST_KNOB_ONE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(169.85, 39.743)), module, BD_cnCt::TEST_KNOB_THREE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(19.893, 43.597)), module, BD_cnCt::OFFSET_KNOB_ZERO_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(70.326, 44.227)), module, BD_cnCt::OFFSET_KNOB_TWO_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(51.966, 45.073)), module, BD_cnCt::OFFSET_KNOB_ONE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(98.736, 46.118)), module, BD_cnCt::OFFSET_KNOB_THREE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(151.085, 47.866)), module, BD_cnCt::TEST_KNOB_TWO_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(117.557, 48.676)), module, BD_cnCt::TEST_KNOB_ZERO_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(27.505, 52.949)), module, BD_cnCt::MULT_KNOB_ZERO_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(47.556, 53.128)), module, BD_cnCt::MULT_KNOB_ONE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(74.25, 52.922)), module, BD_cnCt::MULT_KNOB_TWO_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(95.123, 53.87)), module, BD_cnCt::MULT_KNOB_THREE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(134.666, 59.524)), module, BD_cnCt::TEST_KNOB_FIVE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(168.898, 60.414)), module, BD_cnCt::TEST_KNOB_SEVEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(70.871, 64.454)), module, BD_cnCt::OFFSET_KNOB_SIX_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(99.71, 64.624)), module, BD_cnCt::OFFSET_KNOB_SEVEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(53.052, 65.537)), module, BD_cnCt::OFFSET_KNOB_FIVE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(23.054, 66.001)), module, BD_cnCt::OFFSET_KNOB_FOUR_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(116.773, 70.714)), module, BD_cnCt::TEST_KNOB_FOUR_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(152.059, 70.794)), module, BD_cnCt::TEST_KNOB_SIX_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(47.592, 72.928)), module, BD_cnCt::MULT_KNOB_FIVE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(77.059, 73.177)), module, BD_cnCt::MULT_KNOB_SIX_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(96.783, 73.253)), module, BD_cnCt::MULT_KNOB_SEVEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(135.586, 81.203)), module, BD_cnCt::TEST_KNOB_NINE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(170.014, 84.513)), module, BD_cnCt::TEST_KNOB_ELEVEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(100.847, 85.736)), module, BD_cnCt::OFFSET_KNOB_ELEVEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(72.862, 86.482)), module, BD_cnCt::OFFSET_KNOB_TEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(52.673, 87.183)), module, BD_cnCt::OFFSET_KNOB_NINE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(22.679, 87.711)), module, BD_cnCt::OFFSET_KNOB_EIGHT_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(152.188, 92.688)), module, BD_cnCt::TEST_KNOB_TEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(115.469, 93.25)), module, BD_cnCt::TEST_KNOB_EIGHT_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(95.004, 94.037)), module, BD_cnCt::MULT_KNOB_ELEVEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(76.945, 94.741)), module, BD_cnCt::MULT_KNOB_TEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(28.448, 96.547)), module, BD_cnCt::MULT_KNOB_EIGHT_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(47.317, 96.941)), module, BD_cnCt::MULT_KNOB_NINE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(24.904, 107.186)), module, BD_cnCt::OFFSET_KNOB_TWELVE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(133.81, 106.809)), module, BD_cnCt::TEST_KNOB_THIRTEEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(171.411, 106.658)), module, BD_cnCt::TEST_KNOB_FIFTEEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(98.52, 107.819)), module, BD_cnCt::OFFSET_KNOB_FIFTEEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(72.333, 108.939)), module, BD_cnCt::OFFSET_KNOB_FOURTEEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(54.208, 111.029)), module, BD_cnCt::OFFSET_KNOB_THIRTEEN_PARAM));
+
+		// addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(116.678, 31.609)), module, BD_cnCt::TEST_ENABLE_BUTTON_PARAM));
+		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(116.678, 31.609)), module, BD_cnCt::TEST_ENABLE_BUTTON_PARAM, BD_cnCt::TEST_LIGHT));
+
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(117.557, 48.676)),  module, BD_cnCt::TEST_KNOB_ZERO_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(134.017, 37.179)),  module, BD_cnCt::TEST_KNOB_ONE_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(151.085, 47.866)),  module, BD_cnCt::TEST_KNOB_TWO_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(169.85, 39.743)),   module, BD_cnCt::TEST_KNOB_THREE_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(116.773, 70.714)),  module, BD_cnCt::TEST_KNOB_FOUR_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(134.666, 59.524)),  module, BD_cnCt::TEST_KNOB_FIVE_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(152.059, 70.794)),  module, BD_cnCt::TEST_KNOB_SIX_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(168.898, 60.414)),  module, BD_cnCt::TEST_KNOB_SEVEN_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(115.469, 93.25)),   module, BD_cnCt::TEST_KNOB_EIGHT_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(135.586, 81.203)),  module, BD_cnCt::TEST_KNOB_NINE_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(152.188, 92.688)),  module, BD_cnCt::TEST_KNOB_TEN_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(170.014, 84.513)),  module, BD_cnCt::TEST_KNOB_ELEVEN_PARAM));
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(114.808, 114.321)), module, BD_cnCt::TEST_KNOB_TWELVE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(94.364, 114.865)), module, BD_cnCt::MULT_KNOB_FIFTEEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(30.175, 116.024)), module, BD_cnCt::MULT_KNOB_TWELVE_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(77.272, 115.789)), module, BD_cnCt::MULT_KNOB_FOURTEEN_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(133.81, 106.809)),  module, BD_cnCt::TEST_KNOB_THIRTEEN_PARAM));
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(151.828, 116.827)), module, BD_cnCt::TEST_KNOB_FOURTEEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(50.981, 119.031)), module, BD_cnCt::MULT_KNOB_THIRTEEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(27.617, 242.641)), module, BD_cnCt::MULT_KNOB_FOUR_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(171.411, 106.658)), module, BD_cnCt::TEST_KNOB_FIFTEEN_PARAM));
+
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(19.893, 43.597)),  module, BD_cnCt::OFFSET_KNOB_ZERO_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(51.966, 45.073)),  module, BD_cnCt::OFFSET_KNOB_ONE_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(70.326, 44.227)),  module, BD_cnCt::OFFSET_KNOB_TWO_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(98.736, 46.118)),  module, BD_cnCt::OFFSET_KNOB_THREE_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(23.054, 66.001)),  module, BD_cnCt::OFFSET_KNOB_FOUR_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(53.052, 65.537)),  module, BD_cnCt::OFFSET_KNOB_FIVE_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(70.871, 64.454)),  module, BD_cnCt::OFFSET_KNOB_SIX_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(99.71, 64.624)),   module, BD_cnCt::OFFSET_KNOB_SEVEN_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(22.679, 87.711)),  module, BD_cnCt::OFFSET_KNOB_EIGHT_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(52.673, 87.183)),  module, BD_cnCt::OFFSET_KNOB_NINE_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(72.862, 86.482)),  module, BD_cnCt::OFFSET_KNOB_TEN_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(100.847, 85.736)), module, BD_cnCt::OFFSET_KNOB_ELEVEN_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(24.904, 107.186)), module, BD_cnCt::OFFSET_KNOB_TWELVE_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(54.208, 111.029)), module, BD_cnCt::OFFSET_KNOB_THIRTEEN_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(72.333, 108.939)), module, BD_cnCt::OFFSET_KNOB_FOURTEEN_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(98.52, 107.819)),  module, BD_cnCt::OFFSET_KNOB_FIFTEEN_PARAM));
+
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(27.505, 52.949)),  module, BD_cnCt::MULT_KNOB_ZERO_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(47.556, 53.128)),  module, BD_cnCt::MULT_KNOB_ONE_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(74.25, 52.922)),   module, BD_cnCt::MULT_KNOB_TWO_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(95.123, 53.87)),   module, BD_cnCt::MULT_KNOB_THREE_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(27.617, 242.641)), module, BD_cnCt::MULT_KNOB_FOUR_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(47.592, 72.928)),  module, BD_cnCt::MULT_KNOB_FIVE_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(77.059, 73.177)),  module, BD_cnCt::MULT_KNOB_SIX_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(96.783, 73.253)),  module, BD_cnCt::MULT_KNOB_SEVEN_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(28.448, 96.547)),  module, BD_cnCt::MULT_KNOB_EIGHT_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(47.317, 96.941)),  module, BD_cnCt::MULT_KNOB_NINE_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(76.945, 94.741)),  module, BD_cnCt::MULT_KNOB_TEN_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(95.004, 94.037)),  module, BD_cnCt::MULT_KNOB_ELEVEN_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(30.175, 116.024)), module, BD_cnCt::MULT_KNOB_TWELVE_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(50.981, 119.031)), module, BD_cnCt::MULT_KNOB_THIRTEEN_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(77.272, 115.789)), module, BD_cnCt::MULT_KNOB_FOURTEEN_PARAM));
+		addParam(createParamCentered<Trimpot>(mm2px(Vec(94.364, 114.865)), module, BD_cnCt::MULT_KNOB_FIFTEEN_PARAM));
 
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(81.99, 13.75)), module, BD_cnCt::CLOCK_INPUT_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(40.37, 46.593)), module, BD_cnCt::ONE_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(88.497, 47.444)), module, BD_cnCt::THREE_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(63.423, 52.07)), module, BD_cnCt::TWO_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(13.156, 52.627)), module, BD_cnCt::ZERO_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(90.196, 65.583)), module, BD_cnCt::SEVEN_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(40.093, 67.379)), module, BD_cnCt::FIVE_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(65.467, 72.1)), module, BD_cnCt::SIX_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.42, 73.605)), module, BD_cnCt::FOUR_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(88.706, 86.256)), module, BD_cnCt::ELEVEN_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(41.508, 88.675)), module, BD_cnCt::NINE_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(65.728, 94.304)), module, BD_cnCt::TEN_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(16.336, 95.171)), module, BD_cnCt::EIGHT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(88.623, 108.348)), module, BD_cnCt::FIFTEEN_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(44.476, 112.598)), module, BD_cnCt::THIRTEEN_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(13.156, 52.627)),  module, BD_cnCt::ZERO_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(40.37, 46.593)),   module, BD_cnCt::ONE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(63.423, 52.07)),   module, BD_cnCt::TWO_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(88.497, 47.444)),  module, BD_cnCt::THREE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.42, 73.605)),   module, BD_cnCt::FOUR_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(40.093, 67.379)),  module, BD_cnCt::FIVE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(65.467, 72.1)),    module, BD_cnCt::SIX_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(90.196, 65.583)),  module, BD_cnCt::SEVEN_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(16.336, 95.171)),  module, BD_cnCt::EIGHT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(41.508, 88.675)),  module, BD_cnCt::NINE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(65.728, 94.304)),  module, BD_cnCt::TEN_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(88.706, 86.256)),  module, BD_cnCt::ELEVEN_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(18.469, 113.772)), module, BD_cnCt::TWELVE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(44.476, 112.598)), module, BD_cnCt::THIRTEEN_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(66.927, 116.257)), module, BD_cnCt::FOURTEEN_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(88.623, 108.348)), module, BD_cnCt::FIFTEEN_OUTPUT));
 	}
 };
 
